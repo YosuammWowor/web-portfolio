8e79821678565ebf53bc6709bc6151776a6fca43
Yosua <yosuammwowor@gmail.com>
2025-06-26 07:41:56 +0800

    Add: Halaman Skills and animation

--- home/script.js
+++ home/script.js
@@ -1,10 +1,17 @@
 $("document").ready(function () {
   // about section
-  $("#about").css({ opacity: 0 });
-  $("#about").on("mouseenter", function () {
-    $("#about").animate({ opacity: 1 });
-  });
-  $("#about").on("mouseleave", function () {
-    $("#about").animate({ opacity: 0 });
-  });
+  sectionBlur("#about");
+
+  // about section
+  sectionBlur("#skill");
 });
+
+function sectionBlur(id) {
+  $(id).css({ opacity: 0 });
+  $(id).on("mouseenter", function () {
+    $(id).animate({ opacity: 1 });
+  });
+  $(id).on("mouseleave", function () {
+    $(id).animate({ opacity: 0 });
+  });
+}
